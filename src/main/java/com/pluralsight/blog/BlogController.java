c980ac139848f2f3f8766ccfedbb58663dee3ca3
Dagach <josue.dagach@accenture.com>
2021-05-28 16:41:18 +0200

    :D

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -8,6 +8,7 @@ import com.pluralsight.blog.model.Post;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +22,9 @@ public class BlogController {
     }
     public BlogController(PostRepository postRepository) {
         this.postRepository = postRepository;
+    }
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap) {
+        return "post-details";
     }
 }
